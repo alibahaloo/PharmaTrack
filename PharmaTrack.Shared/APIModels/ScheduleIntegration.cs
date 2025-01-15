c9a4d860eaefc0e8f185dd9522c7b6c760455412
Ali Bahaloo <alibahaloo@gmail.com>
2025-01-15 10:24:36 -0800

    Schedule API blank added

---
+++ PharmaTrack.Shared/APIModels/ScheduleIntegration.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaTrack.Shared.APIModels
+{
+    public class ScheduleIntegration
+    {
+        [Required]
+        public string UserName { get; set; } = default!;
+        [Required]
+        public DateTime Start { get; set; }
+        [Required]
+        public DateTime End { get; set; }
+        [Required]
+        public string Description { get; set; } = default!;
+    }
+}
---
+++ PharmaTrack.Shared/DBModels/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaTrack.Shared.DBModels
+{
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; } = default!;
+        [Required]
+        public DateTime Start {  get; set; }
+        [Required]
+        public DateTime End { get; set; }
+        [Required]
+        public string Description { get; set; } = default!;
+    }
+}
