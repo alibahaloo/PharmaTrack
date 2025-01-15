de3a83c78f420c5e0b39ab451e81ad4e907497bd
Ali Bahaloo <alibahaloo@gmail.com>
2025-01-15 10:37:26 -0800

    remove demo code

---
+++ Schedule.API/Controllers/SchedulesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Schedule.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedulesController : ControllerBase
+    {
+    }
+}
