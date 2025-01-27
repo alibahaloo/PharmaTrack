809bc2ce52136758238fe8a329d86fdc3394d49c
Ali Bahaloo <alibahaloo@gmail.com>
2025-01-27 10:11:05 -0800

    double click on data grid

--- PharmaTrack.WPF/Controls/InventoryControl.xaml.cs
+++ PharmaTrack.WPF/Controls/InventoryControl.xaml.cs
@@ -14,5 +14,17 @@ namespace PharmaTrack.WPF.Controls
             DataContext = viewModel;
             Loaded += async (_, _) => await viewModel.LoadProductsAsync();
         }
+
+        private void DataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (DataContext is InventoryViewModel viewModel && viewModel.ViewProductCommand != null)
+            {
+                // Ensure a product is selected and pass its ID to the command
+                if (viewModel.SelectedProduct != null)
+                {
+                    viewModel.ViewProductCommand.Execute(viewModel.SelectedProduct.Id);
+                }
+            }
+        }
     }
 }
