c05509929612ed6e441e63e1b7bea0c479dfd04a
Ali Bahaloo <alibahaloo@gmail.com>
2025-02-25 23:01:00 -0800

    converter for HasInteractions

---
+++ PharmaTrack.WPF/Converters/InteractionToBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PharmaTrack.WPF.Converters
+{
+    public class InteractionToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool hasInteraction)
+            {
+                return hasInteraction ? Brushes.Red : Brushes.Green;
+            }
+            return Brushes.Green; // default value if not a bool
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
